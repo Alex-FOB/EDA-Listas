55b9378acc2fc614a93960cd702905cb3c885c7b
Alex-FOB <103607209+Alex-FOB@users.noreply.github.com>
2022-09-11 21:24:29 -0300

    Add files via upload

---
+++ Ejercicio 1/Lista Secuencial/main.py
@@ -0,0 +1,10 @@
+from listaSecuencialContenido import ListaSecuencialCont
+
+if __name__ == '__main__':
+    lista = ListaSecuencialCont(4)
+    lista.crear()
+    lista.insertar(1)
+    lista.insertar(4)
+    lista.insertar(3)
+    lista.insertar(2)
+    lista.suprimir(0)
